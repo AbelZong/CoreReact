711c900d2a077c711db3adea22f398c0395eeb5a
abelzong <zongq890630@gmail.com>
2016-10-07 09:52:23 +0800

    abel company test

--- routes/Admin/Company/containers/Toolbar.js
+++ routes/Admin/Company/containers/Toolbar.js
@@ -1,13 +1,32 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import styles from './Shop.scss'
+import styles from './Company.scss'
+import { Input, Button } from 'antd'
+const InputGroup = Input.Group
+import classNames from 'classnames'
 
 class Toolbar extends React.Component {
 
   render() {
+    const { size, placeholder } = this.props
+    const btnCls = classNames({
+      'ant-search-btn': true,
+      'ant-search-btn-noempty': !!this.state.value.trim()
+    })
+    const searchCls = classNames({
+      'ant-search-input': true,
+      'ant-search-input-focus': this.state.focus
+    })
     return (
       <div className={styles.toolbars}>
-        toolbar
+        <InputGroup className={searchCls}>
+          <Input placeholder={placeholder} value={this.state.value} onChange={this.handleInputChange}
+            onFocus={this.handleFocusBlur} onBlur={this.handleFocusBlur} onPressEnter={this.handleSearch}
+          />
+          <div className='ant-input-group-wrap'>
+            <Button icon='search' className={btnCls} size={size} onClick={this.handleSearch} />
+          </div>
+        </InputGroup>
       </div>
     )
   }
--- routes/Admin/Company/index.js
+++ routes/Admin/Company/index.js
@@ -3,13 +3,13 @@ import {
 } from 'store/reducers'
 
 export default (store) => ({
-  path: 'shops',
+  path: 'Company',
   getComponent(nextState, cb) {
     require.ensure([], (require) => {
-      const Shop = require('./containers/Container').default
+      const Company = require('./containers/Container').default
       const reducers = require('./modules/reducers').default
       injectReducers(store, reducers)
-      cb(null, Shop)
-    }, 'adminShops')
+      cb(null, Company)
+    }, 'adminCompany')
   }
 })
